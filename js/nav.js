aedce49d61573e24f59a13a2ceb96c5dd3d9257d
pb-aj <aj.devaux02@gmail.com>
2025-08-03 12:20:48 -0400

    launching first test

---
+++ js/nav.js
@@ -0,0 +1,5 @@
+function bar_dropdown(x) {
+    x.classList.toggle("change");
+    
+    document.getElementById('main_dropdown').style.display = document.getElementById('main_dropdown').style.display =='block' ?   'none' : 'block';
+}
